e92f1518fd50d5e8384a5576c014c8853168cb10
Red-French <red@redfrench.net>
2016-02-15 17:28:07 -0600

    re-write server to use Express

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http');  // require http module
-var server = http.createServer ( function (req, res) {
-     res.writeHead(200, {'Content-Type':
-     'text/html'});
-     res.end('<h1>Hello World</h1>')
+'use strict';
+
+const express = require('express');
+const app = express();
+const routes = require('./routes/');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  // console.log('yes sir');
 });
-var port = Number(process.env.PORT || 3000);  // dynamic port; it reads off the environment variable or set it to 3000
-server.listen(port);
+
+// var server = http.createServer ( function (req, res) {
+//      res.writeHead(200, {'Content-Type':
+//      'text/html'});
+//      res.end('<h1>Hello World</h1>')
+// });
+// var port = Number(process.env.PORT || 3000);  // dynamic port; it reads off the environment variable or set it to 3000
+// server.listen(port);
