f12b14f2101f8db573b460c6a07aaba1725cacf1
Red-French <red@redfrench.net>
2016-02-16 17:00:04 -0600

    no changes

--- server.js
+++ server.js
@@ -2,21 +2,17 @@
 
 const express = require('express');
 const app = express();
+
 const routes = require('./routes/');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  // dynamic port; reads off envionment variable or sets port to 3000
+
+app.set('view engine', 'jade');
+
 
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log('hey, from server.js!!');
 });
-
-// var server = http.createServer ( function (req, res) {
-//      res.writeHead(200, {'Content-Type':
-//      'text/html'});
-//      res.end('<h1>Hello World</h1>')
-// });
-// var port = Number(process.env.PORT || 3000);  // dynamic port; it reads off the environment variable or set it to 3000
-// server.listen(port);
