f340eff34cda47be97c0754e39b3718920b876ee
Red-French <red@redfrench.net>
2016-02-16 00:32:19 -0600

    initial commit

--- routes/default.js
+++ routes/default.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/default', (req, res) => {
-  res.send("I'm workin here");
+  res.send('I\'m workin here');
 });
 
 module.exports = router;
